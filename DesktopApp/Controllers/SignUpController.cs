aa64c2bef1c30ea52cc11ea36400c72de7cf634f
elmarmaan <elmarmaan@gmail.com>
2014-05-09 13:17:01 +0200

    fixed project and added connectionstring for release

--- DesktopApp/Controllers/SignUpController.cs
+++ DesktopApp/Controllers/SignUpController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Contexts;
 using Domain;
 
 namespace DesktopApp.Controllers
@@ -14,6 +15,15 @@ namespace DesktopApp.Controllers
 
         public ActionResult Index()
         {
+            var db = new LetsPlayDartsContext();
+            var account = new Account
+            {
+                EmailAddress = "test",
+                Password = "test",
+                Name = "test"
+            };
+            db.Accounts.Add(account);
+            db.SaveChanges();
             return View();
         }
 

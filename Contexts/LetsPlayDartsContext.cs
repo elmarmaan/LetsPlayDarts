94316b7f8dc84b58aec9f2a3dd7398e915ea9fcc
elmarmaan <elmarmaan@gmail.com>
2014-05-09 14:20:19 +0200

    fixed project

---
+++ Contexts/LetsPlayDartsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Contexts
+{
+    public class LetsPlayDartsContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; } 
+    }
+}

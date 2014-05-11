0aa0622befd8520af5129ce577ceca6d8da758b2
elmarmaan <elmarmaan@gmail.com>
2014-05-11 21:28:16 +0200

    added log out

---
+++ DesktopApp/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain;
+using Services;
+using Services.Interfaces;
+
+namespace DesktopApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public AccountController()
+        {
+            _accountService = new AccountService();
+        }
+
+        [HttpGet]
+        public ActionResult AddAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAccount(Account account)
+        {
+            if (ViewData.ModelState.IsValid)
+            {
+                _accountService.AddAccount(account);
+            }
+            return RedirectToAction("GetAccounts");
+        }
+
+        [Authorize]
+        public ActionResult GetAccounts()
+        {
+            var accounts = _accountService.GetAccounts();
+            ViewData.Model = accounts;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditAccount(long accountId)
+        {
+            var account = _accountService.GetAccount(accountId);
+            ViewData.Model = account;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditAccount(Account account)
+        {
+            _accountService.EditAccount(account);
+            return RedirectToAction("GetAccounts");
+        }
+
+        public ActionResult DeleteAccount(long accountId)
+        {
+            _accountService.DeleteAccount(accountId);
+            return RedirectToAction("GetAccounts");
+        }
+
+        public ActionResult GetAccount(long accountId)
+        {
+            var account = _accountService.GetAccount(accountId);
+            ViewData.Model = account;
+            return View();
+        }
+    }
+}

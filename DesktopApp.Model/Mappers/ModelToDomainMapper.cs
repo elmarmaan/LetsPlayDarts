098b83ab3ba22389c67c38241c8fb39458958c0d
elmarmaan <elmarmaan@gmail.com>
2014-05-11 11:59:58 +0200

    fixed git reference for desktopapp.model

---
+++ DesktopApp.Model/Mappers/ModelToDomainMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DesktopApp.Model.Mappers
+{
+    public static class ModelToDomainMapper
+    {
+        public static Account Map(LoginModel model)
+        {
+            return new Account
+            {
+                EmailAddress = model.EmailAddress,
+                Password = model.Password
+            };
+        }
+    }
+}
--- DesktopApp.Model/Properties/AssemblyInfo.cs
+++ DesktopApp.Model/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b49c226b-1800-408c-92ea-c9b28addd876")]
+[assembly: Guid("479a76bb-dfdd-49ce-b922-e1c7bcee253c")]
 
 // Version information for an assembly consists of the following four values:
 //

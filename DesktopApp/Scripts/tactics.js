48c84fcdf2a1ebcad81eb155d019ff97c1844faa
elmarmaan <elmarmaan@gmail.com>
2014-05-23 09:06:13 +0200

    added score input table first version

---
+++ DesktopApp/Scripts/tactics.js
@@ -0,0 +1,12 @@
+﻿var _turn = 0;
+
+function changeTurn() { 
+    $('.scoreboard .player_name td').removeClass('turn');
+    if (_turn === 0) {
+        _turn++;
+    } else {
+        _turn--;
+    }
+    var playersTurn = $('.scoreboard .player_name td')[_turn];
+    $(playersTurn).addClass('turn');
+}
